3fc0cd5da317af4e9e85e59053c7ba39247639a3
Ina <s9528788@gmail.com>
2023-08-02 22:50:38 +0800

    add test and insertable

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -2,17 +2,63 @@ package app;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.sql.Insert;
 
 import core.util.HibernateUtil;
 import web.member.pojo.Member;
 
 public class TestApp {
+
 	public static void main(String[] args) {
+//		Member member = new Member();
+//		member.setUsername("使用者名稱");
+//		member.setPassword("密碼");
+//		member.setNickname("暱稱");
+		TestApp app = new TestApp();
+//		app.insert(member);
+//		System.out.println(member.getId());
+
+		System.out.println(app.deleteByID(3));
+	}
+
+	public Integer insert(Member member) {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
-		Member member = session.get(Member.class, 1);
-		System.out.println(member.getNickname());
-				
-		HibernateUtil.shutdown();
+		try {
+			Transaction transaction = session.beginTransaction();
+			session.persist(member);
+			transaction.commit();
+			return member.getId();
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return null;
+		}
 	}
+
+	public int deleteByID(Integer id) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member member = session.get(Member.class, id);
+			session.remove(member);
+			transaction.commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return -1;
+		}
+	}
+
+//      ---- 測試是否能與資料庫連線成功並取得資料 ----
+//		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//		Session session = sessionFactory.openSession();
+//		Member member = session.get(Member.class, 1);
+//		System.out.println(member.getNickname());
+//				
+//		HibernateUtil.shutdown();
+
 }
